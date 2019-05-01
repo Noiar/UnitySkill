a5d362385bc3776ae5c2f7ee2fedc10e1f1ac616
Noiar <dkckacka1@naver.com>
2019-05-01 17:03:48 +0900

    공부한 php 파일 추가

---
+++ UnitySkill/Assets/SQLConnect/sqlconnect/savedata.php
@@ -0,0 +1,28 @@
+<?php
+	$con = mysqli_connect('localhost','root','root','unityaccess');
+
+	if(mysqli_connect_errno())
+	{
+		echo "1: Connection failed";
+		exit();
+	}
+
+	$username = $_POST["name"];
+	$newscore = $_POST["score"];
+
+	$namecheckquery = "SELECT username FROM player WHERE username='" . $username ."';";
+
+	$namecheck = mysqli_query($con, $namecheckquery) or die("2: Name check query failed");
+
+	if(mysqli_num_rows($namecheck) != 1)
+	{
+		echo "5: Either no user with name, or more than one"; 
+		exit();
+	}
+
+	$updatequery = "UPDATE player SET score = " . $newscore . " WHERE username = '" . $username . "';";
+
+	mysqli_query($con, $updatequery) or die("7: Save query failed");
+
+	echo "0";
+?>
---
+++ UnitySkill/Assets/SQLConnect/sqlconnect/webtest.php
@@ -0,0 +1,4 @@
+<?php
+	
+	echo "webtest" . "\t" . 500;
+?>
