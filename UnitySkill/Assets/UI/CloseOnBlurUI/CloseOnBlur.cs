3af78ef2a2db455bcb1bbf7efe3deacce14d8290
Noiar <dkckacka1@naver.com>
2019-05-02 13:59:55 +0900

    CloseOnBlur

---
+++ UnitySkill/Assets/UI/CloseOnBlurUI/CloseOnBlur.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseOnBlur : MonoBehaviour
+{
+
+    bool mouseHover;
+
+    public void SetHover(bool hover)
+    {
+        mouseHover = hover;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0) && !mouseHover)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ UnitySkill/Assets/UI/CloseOnBlurUI/CloseOnBlur_2.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class CloseOnBlur_2 : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+    bool mouseHover;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        mouseHover = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        mouseHover = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0) && !mouseHover)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
